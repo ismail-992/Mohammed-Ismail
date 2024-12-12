0013dad14dd28233794dee4aa960dcbc7de2c55e
ismail-992 <mohammedismail9621@gmail.com>
2024-12-12 19:09:39 +0530

    First commit

---
+++ simplejava.java
@@ -0,0 +1,26 @@
+// Java program for basic arithmetic operations
+public class simplejava {
+    public static void main(String[] args) {
+        int num1 = 20, num2 = 10;
+
+       
+        int addition = num1 + num2;
+        System.out.println("Addition: " + num1 + " + " + num2 + " = " + addition);
+
+
+        int subtraction = num1 - num2;
+        System.out.println("Subtraction: " + num1 + " - " + num2 + " = " + subtraction);
+
+        
+        int multiplication = num1 * num2;
+        System.out.println("Multiplication: " + num1 + " * " + num2 + " = " + multiplication);
+
+       
+        int division = num1 / num2;
+        System.out.println("Division: " + num1 + " / " + num2 + " = " + division);
+
+       
+        int modulo = num1 % num2;
+        System.out.println("Modulo: " + num1 + " % " + num2 + " = " + modulo);
+    }
+}
